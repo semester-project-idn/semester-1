99b66f333db3259768326d27c6e4f6b7ce8aa6af
faridanaksoleh <itspuyd@gmail.com>
2025-09-04 23:07:23 +0700

    feat: add hello.php for first PHP script

---
+++ php/hello.php
@@ -0,0 +1,9 @@
+<?php
+    //hello.php
+    //Tujuan: Menampilkan teks Hello World di browser
+    
+    echo "Hello world <br>";
+    $nama = "Farid";                            
+    echo "Hello $nama, Selamat datang di PHP";     
+     //Jalankan server dengan php -S localhost:8000. --> Akses file via browser. http://localhost:8000/hello.php
+?>
